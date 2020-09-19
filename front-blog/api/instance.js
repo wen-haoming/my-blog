99b523ce90db01d63f3feeb9d63df30acdec0814
wenhaoming <435203093@qq.com>
2020-09-19 09:49:19 +0800

    fix:修改配置文件

--- front-blog/api/instance.js
+++ front-blog/api/instance.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 // import axios from "axios";
 
 const config = {};
-  if(process.env.NODE_ENV === 'development'){
     if (typeof window === "undefined") {
       //服务器
       config.baseURL = "http://127.0.0.1:7002/";
@@ -12,9 +11,7 @@ const config = {};
       config.baseURL = "http://127.0.0.1:7002/";
     //   config.baseURL = "http://yuanjin.tech:5005/";
     }
-  }else{
-      config.baseURL = "http://47.107.232.30/";
-  }
+
  
 console.log(process.env,'=========================')
 
